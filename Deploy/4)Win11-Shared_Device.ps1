dbcb34c57c31693de2a7d4c3f4e05986952a1170
Casey Davis <casey.davis@coloradoearlycolleges.org>
2026-07-08 09:39:00 -0600

    Added Shared Task Sequence and rearranged numbering

---
+++ Deploy/4)Win11-Shared_Device.ps1
@@ -0,0 +1,122 @@
+#Script to deploy Windows 11 A3 Staff Devices
+
+# Prompt the user to enter the Asset Tag number
+do {
+    $assetTag = Read-Host "Please enter the asset tag number (4 to 5 digit number)"
+    if ($assetTag -match '^\d{4,5}$') {
+        $assetTag | Out-File -FilePath "X:\sources\AssetTag.txt" -Encoding ascii -Force
+    }
+} while ($assetTag -notmatch '^\d{4,5}$')
+Write-Output "You entered a valid asset tag number: $assetTag"
+
+#=======================================================================
+#   [OS] Params and Start-OSDCloud
+#=======================================================================
+$Params = @{
+    OSVersion  = "Windows 11"
+    OSBuild    = "25H2"
+    OSEdition  = "Pro"
+    OSLanguage = "en-us"
+    OSLicense  = "Volume"
+    ZTI        = $true
+    Firmware   = $true
+}
+Start-OSDCloud @Params
+
+#================================================
+#  [PostOS] AutopilotOOBE Configuration Staging
+#================================================
+$Serial = Get-WmiObject Win32_bios | Select-Object -ExpandProperty SerialNumber
+$AssignedComputerName = "CEC-$Serial"
+
+Write-Host -ForegroundColor Green "Create C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json"
+$AutopilotOOBEJson = @"
+{
+    "AssignedComputerName" : "$AssignedComputerName",
+    "AddToGroup":  "Autopilot - Device - Staff Shared Device Win11",
+    "Assign":  {
+                   "IsPresent":  true
+               },
+    "GroupTag":  "Staff",
+    "Hidden":  [
+                   "AddToGroup",
+                   "AssignedUser",
+                   "PostAction",
+                   "GroupTag",
+                   "Assign",
+                   "Docs"
+               ],
+    "PostAction":  "Restart",
+    "Run":  "NetworkingWireless",
+    "Title":  "CEC Autopilot Manual Register"
+}
+"@
+
+If (!(Test-Path "C:\ProgramData\OSDeploy")) {
+    New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
+}
+$AutopilotOOBEJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json" -Encoding ascii -Force
+
+#================================================
+#  [PostOS] OOBE CMD Command Line
+#================================================
+Invoke-RestMethod https://raw.githubusercontent.com/Colorado-Early-Colleges/osdcloud/refs/heads/main/Set-LenovoAssetTag.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\set-lenovoassettag.ps1' -Encoding ascii -Force
+Invoke-RestMethod https://raw.githubusercontent.com/Colorado-Early-Colleges/osdcloud/refs/heads/main/Autopilot.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\autopilot.ps1' -Encoding ascii -Force
+$OOBECMD = @'
+@echo off
+
+# Prompt for setting Lenovo Asset Tag
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\set-lenovoassettag.ps1
+# Commented out because App Secret based Autopilot Enrollment Configured
+# start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\autopilot.ps1
+
+# Below a PS session for debug and testing in system context, # when not needed
+# start /wait powershell.exe -NoL -ExecutionPolicy Bypass
+
+exit
+'@
+$OOBECMD | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.cmd' -Encoding ascii -Force
+
+#================================================
+#  [PostOS] SetupComplete CMD Command Line
+#================================================
+Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
+$SetupCompleteCMD = @'
+powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\rename-computer.ps1
+'@
+$SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
+
+$UnattendXml = @'
+<?xml version="1.0" encoding="utf-8"?>
+<unattend xmlns="urn:schemas-microsoft-com:unattend">
+    <settings pass="specialize">
+        <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <RunSynchronous>
+                <RunSynchronousCommand wcm:action="add">
+                    <Order>1</Order>
+                    <Description>Start Autopilot Import & Assignment Process</Description>
+                    <Path>PowerShell -ExecutionPolicy Bypass C:\Windows\Setup\scripts\autopilot.ps1</Path>
+                </RunSynchronousCommand>
+            </RunSynchronous>
+        </component>
+    </settings>
+</unattend>
+'@
+
+if (-NOT (Test-Path 'C:\Windows\Panther')) {
+    New-Item -Path 'C:\Windows\Panther'-ItemType Directory -Force -ErrorAction Stop | Out-Null
+}
+
+$Panther = 'C:\Windows\Panther'
+$UnattendPath = "$Panther\Unattend.xml"
+$UnattendXml | Out-File -FilePath $UnattendPath -Encoding utf8 -Width 2000 -Force
+
+Write-Host "Copying USB Drive Scripts"
+Copy-Item X:\sources\ C:\OSDCloud\ -Recurse -Force
+
+#=======================================================================
+#   Restart-Computer
+#=======================================================================
+Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
